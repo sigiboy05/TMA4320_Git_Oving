4a335b1920e5d8aaaa36ed597ee0374b7637cf3d
Elling Svee <86355496+ellingsvee@users.noreply.github.com>
2026-01-13 20:31:51 +0100

    Some comments related to the asserts

--- pytest_example/tests/test_area_simple.py
+++ pytest_example/tests/test_area_simple.py
@@ -10,14 +10,21 @@ from area import circle_area
 @pytest.mark.parametrize(
     "radius",
     [
-        1,
-        2,
         0.5,
+        2.0,
+        1.0
     ],
 )
 def test_circle_area_simple(radius):
     """Area of circle should be pi * r^2."""
     expected = np.pi * radius**2
+
+    # # What is an assert?
+    # assert 0 == 1 # False -> Code will fail
+    # assert 1 == 1 # True -> Code will pass
+
+    # Actual test
+    # assert circle_area(radius) == expected, f"circle_area failed for radius={radius}"
     assert np.allclose(circle_area(radius), expected), (
         f"circle_area failed for radius={radius}"
     )
